96989fc52b12f6e52ad3e9ac327f8c0ea4cde488
raspuna <raspuna@gmail.com>
2022-02-07 14:08:29 -0800

    first java program assignment

---
+++ Week1/Extra Practice/FirstJavaProgram/FirstJavaProgram.java
@@ -0,0 +1,8 @@
+public class FirstJavaProgram {
+    public static void main(String[] args) {
+        System.out.println("My name is Kyeongeun Choi");
+        System.out.println("I am 38 yearls old");
+        System.out.println("My hometown is Seoul, South Korea");
+    }
+    
+}
