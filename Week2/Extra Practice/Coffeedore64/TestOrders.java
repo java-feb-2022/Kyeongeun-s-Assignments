607206a48a6ef4c6785c827a093ce48b319b2e1a
raspuna <raspuna@gmail.com>
2022-02-14 22:32:52 -0800

    add some comment

--- Week2/Extra Practice/Coffeedore64/TestOrders.java
+++ Week2/Extra Practice/Coffeedore64/TestOrders.java
@@ -5,12 +5,17 @@ public class TestOrders {
     public static void main(String[] args) {
         CoffeeKiosk myKiosk = new CoffeeKiosk();
 
+        // default menu
         myKiosk.addMenuItem("banana", 2);
         myKiosk.addMenuItem("coffee", 1.5);
         myKiosk.addMenuItem("latte", 4.5);
         myKiosk.addMenuItem("capuccino", 3);
         myKiosk.addMenuItem("muffin", 4);
+
+        // manual input
         myKiosk.addMenuItemByInput();
+
+        // orders by customer.
         for (int i = 0; i < 4; i++) {
             
             myKiosk.newOrder();
