2bfe4a9d14c7f2a596e8a1d5c44b6a7078dbc560
raspuna <raspuna@gmail.com>
2022-02-15 17:57:15 -0800

    add some comment

--- Week2/Zoo Keeper Part 2/src/zookeeper/BatTest.java
+++ Week2/Zoo Keeper Part 2/src/zookeeper/BatTest.java
@@ -3,13 +3,18 @@ package zookeeper;
 public class BatTest {
     public static void main(String[] args) {
         Bat dracula = new Bat();
+        // attack town 3 times
         dracula.attackTown();
         dracula.attackTown();
         dracula.attackTown();
         dracula.displayEnergy();
+
+        // eat humans 2 times
         dracula.eatHumans();
         dracula.eatHumans();
         dracula.displayEnergy();
+
+        // fly 2 times
         dracula.fly();
         dracula.fly();
         dracula.displayEnergy();
