55d2dcfe7370572ae434948430890346ade77147
raspuna <raspuna@gmail.com>
2022-02-22 23:32:33 -0800

    daikichi 2

--- Week3/Extra Practice/daikichi/src/main/java/com/kc/daikichi/controllers/DaikichiController.java
+++ Week3/Extra Practice/daikichi/src/main/java/com/kc/daikichi/controllers/DaikichiController.java
@@ -1,5 +1,6 @@
 package com.kc.daikichi.controllers;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,6 +18,19 @@ public class DaikichiController {
 	@RequestMapping("/daikichi/tomorrow")
 	public String tomorrow() {
 		return "Tomorrow, an opportunity will arise, so be sure to be open to new ideas!";
+	}
+	@RequestMapping("/daikichi/travel/{city}")
+	public String travel(@PathVariable String city) {
+		return "Congratulations! You will soon travel to "+city + "!";
+	}
+	@RequestMapping("/daikichi/lotto/{num}")
+	public String lotto(@PathVariable Integer num) {
+		
+		if (num % 2 == 0) {
+			return "You will take a grand journey in the near future, but be weary of tempting offers";
+		} else {
+			return "You have enjoyed the fruits of your labor but now is a great time to spend time with family and friends";
+		}
 	}
 	
 }
